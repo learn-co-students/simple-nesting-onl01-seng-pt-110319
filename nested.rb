d510ed4b7ff51b216b3c8ba97b215296f13aced8
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-11-30 17:59:36 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -100,10 +100,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alan_kay[:alans_new_info] = alan_kay.delete(:known_for) # hash[:new_key] = hash.delete :old_key
      alans_new_info = "GUI"
-
-
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash # hash[:new_key] = hash.delete :old_key
 end
 
 def adding_to_dennis
@@ -123,5 +122,5 @@ def adding_to_dennis
         }
      }
      programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash
 end
